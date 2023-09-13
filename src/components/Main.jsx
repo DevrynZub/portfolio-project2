b0a0c53c036955d037900f5ab5c2756aaee43cc2
Devryn <devryn.zub@gmail.com>
2023-09-13 01:15:33 -0700

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,12 @@ import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 const Main = () => {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover object-left' src="https://images.unsplash.com/photo-1500673922987-e212871fec22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="" />
-      <div className="w-full h-screen absolute top-0 left-0 bg-white/20">
-        <div>
-          <h1>Welcome</h1>
-          <h2>I'm Devryn Zub</h2>
-          <h3>I'm a
+      <img className='w-full h-screen object-cover object-left' src="https://images.unsplash.com/photo-1500673922987-e212871fec22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="/" />
+      <div className='w-full h-screen absolute top-0 left-0 bg-white/20'>
+        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+          <h1 className='sm:text-5xl text-4xl  pb-3 font-bold text-grey-800'>Welcome</h1>
+          <h2 className='sm:text-4xl text-3xl text-grey-800'>I'm Devryn Zub</h2>
+          <h3 className='flex sm:text-2xl text-1xl pt-4 text-grey-800'>I'm a
             <TypeAnimation
               sequence={[
                 'Full-Stack Developer',
@@ -24,15 +24,14 @@ const Main = () => {
               cursor={true}
               speed={50}
               style={{ fontSize: '1em', paddingLeft: '5px', display: 'inline-block' }}
-              repeat={Infinity}
-            />
+              repeat={Infinity} />
           </h3>
-          <div>
+          <div className='flex justify-between pt-5 max-w-[100px] w-full'>
             <a href="https://www.linkedin.com/in/devryn-zub-41164a194/">
-              <FaLinkedinIn className="cursor-pointer" size={20} />
+              <FaLinkedinIn className="cursor-pointer" size={30} />
             </a>
             <a href="https://github.com/DevrynZub">
-              <FaGithub className="cursor-pointer" size={20} />
+              <FaGithub className="cursor-pointer" size={30} />
             </a>
           </div>
         </div>
