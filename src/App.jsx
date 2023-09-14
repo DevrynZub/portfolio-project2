94b5d1c4e9964e1f3aad7023b746533aa2ba8d41
Devryn <devryn.zub@gmail.com>
2023-09-14 01:02:19 -0700

    updates to work, projects next

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <div >
       <Sidenav />
       <Main />
+      <Work />
     </div>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,28 +3,28 @@ import WorkItem from './WorkItem.jsx'
 
 const data = [
   {
-    year: 7.2021 - 2.2023,
+    year: '7.2021 to 2.2023',
     company: 'OpSec Security',
     title: 'Brand Security Analyst',
     details: 'Performed quality analysis and identified trends to ensure data integrity for the client. Used multiple technologies to determine the best course of action for the clients needs. Tracked key metrics and applied them in reports to the client with infractions and violations.'
   },
 
   {
-    year: 8.2019 - 7.2021,
+    year: '8.2019 to 7.2021',
     company: 'Best Buy',
     title: 'SWAT/Inventory Specialist',
     details: 'Checked internal supply and stock each day to insure flow of deliveries and to verify theft. Completed weekly and monthly reports on stock counts and theft to update truck schedules. Completed and led a yearly audit of the entire store to determine the upcoming yearly budget.'
   },
 
   {
-    year: 10.2019 - 7.2021,
+    year: '10.2019 to 7.2021',
     company: 'Geek Squad',
     title: 'Cadet',
     details: 'Worked in customers homes to perform services at clients requests. Cadets were supports for Repair Agents and were in charge of keeping areas clean and taking on projects that would allow the repair agent to make sales and educate the customers. Worked on a very strict timeline of getting work completed as there were multiple jobs scheduled a day.'
   },
 
   {
-    year: 10.2013 - 8.2018,
+    year: '10.2013 to 8.2018',
     company: 'Geek Squad/Best Buy',
     title: 'Repair Agent',
     details: ' Worked directly with clients to educate on technical issues and educated them on upcoming changes. Repaired computers and cleaned up software and infections. Updated software and installed new software as per client requests. Repaired and replaced hardware in laptops and desktops. Adheres to safety protocols and policies to reduce workplace hazards.'
@@ -32,8 +32,17 @@ const data = [
 ]
 const Work = () => {
   return (
-    <div>
-      Work
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+      <h1 className='text-4xl font-bold text-center mb-6 text-red-500'>Work</h1>
+      {data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          // @ts-ignore
+          year={item.year}
+          company={item.company}
+          title={item.title}
+          details={item.details} />
+      ))}
     </div>
   )
 }
