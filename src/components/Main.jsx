5d0f129064956179fd4414f5e2515aee8d6faa3e
Devryn Zub <devryn.zub@gmail.com>
2023-10-03 01:47:15 -0600

    updated background

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 const Main = () => {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover object-left' src="https://images.unsplash.com/photo-1500673922987-e212871fec22?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80" alt="/" />
+      <img className='w-full h-screen object-cover object-left' src="https://images.unsplash.com/photo-1457364887197-9150188c107b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3000&q=80" alt="/" />
       <div className='w-full h-screen absolute top-0 left-0 bg-white/20'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
           <h1 className='sm:text-5xl text-4xl  pb-3 font-bold text-grey-800'>Welcome</h1>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -53,10 +53,10 @@ const Skills = () => {
                         <img className='w-20 mx-auto mt-3' src={Tailwind} alt="HTML icon" />
                         <p className='my-4'>TAILWIND</p>
                     </div>
-                    <div className='text-white bg-slate-700 shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    {/* <div className='text-white bg-slate-700 shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                         <img className='w-20 mx-auto mt-3' src={CSharp} alt="HTML icon" />
                         <p className='my-4'>TAILWIND</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
